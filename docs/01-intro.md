88c8339e5814b72c65d05f9181009bef827a6fb7
Cong Nguyen <5560025+rampadc@users.noreply.github.com>
2024-11-11 23:56:10 +1100

    add intro

---
+++ docs/01-intro.md
@@ -0,0 +1,20 @@
+---
+sidebar_position: 1
+slug: /
+---
+
+# Zero to Production in Go
+
+## Why this Go guide?
+
+I’ve been dipping my toes into Go lately, and I've noticed a gap in the learning resources. Coming from Rust, it has a wealth of excellent guides and tutorials, Go’s landscape is a bit more barren, especially when it comes to practical, production-ready applications for **beginners**.
+
+So, I'm writing this guide to share what I've learned and to help others level up their Go skills. We'll cover everything from the basics to advanced topics, including:
+
+* Building APIs: Designing, testing, and securing RESTful APIs.
+* Data Persistence: Working with SQL and NoSQL databases.
+* Monitoring and Logging: Implementing effective telemetry solutions.
+* Deployment Strategies: Deploying Go applications to production environments.
+* Error Handling and Debugging: Mastering Go's error handling mechanisms.
+
+I'm no expert in Go, so this guide will be updated regularly when techniques or patterns are made avail to me. I hope this resource will help you become a more proficient Go developer.
---
+++ docusaurus.config.ts
@@ -0,0 +1,88 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+const config: Config = {
+  title: 'Zero to Production in Go',
+  tagline: 'Like Zero to Production in Rust, but for Go',
+
+  // Set the production url of your site here
+  url: 'https://rampadc.github.io',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/z2p-in-go/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'rampadc', // Usually your GitHub org/user name.
+  projectName: 'z2p-in-go', // Usually your repo name.
+  trailingSlash: false,
+  githubHost: 'github.com',
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          routeBasePath: '/',
+          sidebarPath: './sidebars.ts',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/rampadc/z2p-in-go',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  markdown: {
+    mermaid: true,
+  },
+
+  themes: ['@docusaurus/theme-mermaid'],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/docusaurus-social-card.jpg',
+    navbar: {
+      title: 'Zero to Production in Go',
+      // logo: {
+      //   alt: 'My Site Logo',
+      //   src: 'img/logo.svg',
+      // },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Docs',
+        },
+        {
+          href: 'github.com/rampadc/z2p-in-go',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
